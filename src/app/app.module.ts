97e06180138909ae13a6dd613d206e7a7ff48c44
khuthy <muendakhuthadzo@gmail.com>
2019-07-02 05:48:40 +0200

    edited my cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { ResumeComponent } from './resume/resume.component';
-
+import { TopnavComponent } from './topnav/topnav.component';
+import { FormsModule } from '@angular/forms';
+  import { from } from 'rxjs';
 
 
 
@@ -15,14 +17,17 @@ import { ResumeComponent } from './resume/resume.component';
     AppComponent,
     ContactMeComponent,
     ResumeComponent,
+    TopnavComponent,
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     RouterModule.forRoot([
-      {path: 'resume', component: ResumeComponent},
-      {path: 'contactMe', component: ContactMeComponent}
+      {path: '', component: ResumeComponent},
+      {path: 'contactMe', component: ContactMeComponent},
+      {path: 'topnav', component: TopnavComponent}
     ])
    
   ],
--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactMeComponent implements OnInit {
 
+  username: string;
+  email: string;
+  message: string = '';
+
+  error: any;
+  success: any;
   constructor() { }
+
+  sendMessage(){
+    if(this.username == '') {
+      this.error = 'username is required';
+    }
+     if(this.email == ''){
+      this.error = 'email is required';
+    }
+    if(this.message == ''){
+      this.error = 'message is required';
+    }
+    if(this.username && this.email && this.message) {
+      this.error = '';
+
+      this.success = 'Thank you ' +this.username+ '. Successfully sent'
+    }
+  }
 
   ngOnInit() {
   }
